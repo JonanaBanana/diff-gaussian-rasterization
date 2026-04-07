d1f5c4f942dc23bc65aa8374f606f391a0eb522d
Jonathan Nielsen <jonathan@majj.dk>
2026-04-07 11:26:04 +0200

    minor fix

--- cuda_rasterizer/forward.cu
+++ cuda_rasterizer/forward.cu
@@ -426,7 +426,7 @@ renderCUDA(
 	// max reduce the last contributor
     typedef cub::BlockReduce<uint32_t, BLOCK_SIZE> BlockReduce;
     __shared__ typename BlockReduce::TempStorage temp_storage;
-    last_contributor = BlockReduce(temp_storage).Reduce(last_contributor, [](int a, int b){ return a > b ? a : b; });
+    last_contributor = BlockReduce(temp_storage).Reduce(last_contributor, cuda::maximum<>{});
     // last_contributor = BlockReduce(temp_storage).Reduce(last_contributor, cub::Max());
 	if (block.thread_rank() == 0) {
 		max_contrib[tile_id] = last_contributor;
